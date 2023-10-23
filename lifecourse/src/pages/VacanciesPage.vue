58480c7269cf6b9025b1d5c306bb31aec4fa0a89
Quadardas <akladskiy@gmail.com>
2023-10-23 22:52:16 +0500

    вакансии

--- lifecourse/src/pages/VacanciesPage.vue
+++ lifecourse/src/pages/VacanciesPage.vue
@@ -5,17 +5,25 @@
       <div class="post">
         <div class="company__info">
           <div class="company__logo">
-            <img src="../img/no-avatar.png" alt="" />
+            <img src="../img/no-image.png" alt="" />
           </div>
           <div class="company__name">**Название Компании**</div>
           <div class="peculiarities"></div>
+          <div class="favorite">
+            <img src="../img/sticker (2).png" alt="" />
+          </div>
         </div>
         <div class="vac__info">
-          <div class="job-title">**Должность**</div>
+          <div class="job-title">
+            **Должность**
+            <div class="job_title__description">**описание должности**</div>
+          </div>
           <div class="schedule">**График**</div>
           <div class="salary">99999 Руб</div>
           <div class="comment">**Комментарий от работодателя**</div>
-          <div class="city">**Город**</div>
+          <div class="city">
+            <img src="../img/sticker (1).png" alt="" />**Город**
+          </div>
         </div>
       </div>
     </div>
@@ -29,16 +37,56 @@ import NavBar from "@/components/NavBar.vue";
 .container {
   display: flex;
   .vacancies__feed {
-    margin: 0 auto;
+    margin: 20px auto;
     height: 100%;
+    background-color: lightgray;
+    padding: 15px 15px;
+    border-radius: 20px;
 
     .post {
       background-color: #fff;
-      width: 500px;
+      width: 450px;
       height: fit-content;
       border-radius: 20px;
       padding: 15px;
-      margin-top: 20px;
+      margin-bottom: 15px;
+
+      .company__info {
+        display: flex;
+        justify-content: space-around;
+        .company__logo {
+          img {
+            width: 40px;
+            height: 40px;
+          }
+        }
+        .favorite {
+          img {
+            width: 30px;
+            height: 30px;
+          }
+        }
+      }
+      .job-title {
+        font-weight: 700;
+      }
+      .schedule {
+        font-weight: 700;
+        margin: 10px 0;
+      }
+      .salary {
+        font-weight: 700;
+        margin: 10px 0;
+      }
+      .city {
+        margin-top: 10px;
+        display: flex;
+        align-items: center;
+        img {
+          width: 30px;
+          height: 30px;
+        }
+      }
     }
   }
 }
